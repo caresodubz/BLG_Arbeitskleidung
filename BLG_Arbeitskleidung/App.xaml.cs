c2d9ec3ba60165dddfc58803e758391dd42d93cb
Tom Merbach <tom.merbach@blg-fashion.de>
2024-08-09 11:08:35 +0200

    Fehlermeldung beim Starten eingebaut, wenn die Verbindung nicht hergestellt wurde oder der User keine Berechtigungen hat

--- BLG_Arbeitskleidung/App.xaml.cs
+++ BLG_Arbeitskleidung/App.xaml.cs
@@ -6,15 +6,24 @@ namespace BLG_Arbeitskleidung {
 
     public partial class App : Application {
         private void OnStartUp(object sender, StartupEventArgs e) {
-            MainWindowView mainWindowView = new MainWindowView();
-            MainWindowViewModel viewModel = new MainWindowViewModel();
-            mainWindowView.DataContext = viewModel;
-            mainWindowView.ShowDialog();
+
+            try {
+                MainWindowView mainWindowView = new MainWindowView();
+                MainWindowViewModel viewModel = new MainWindowViewModel();
+                mainWindowView.DataContext = viewModel;
+                mainWindowView.ShowDialog();
+            }
+            catch(Exception) {
+                MessageBox.Show(
+                    "Fehler beim Verbinden mit der Datenbank!",
+                    "Fehler",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
         }
 
         private void OnExit(object sender, ExitEventArgs e) {
 
         }
     }
-
 }
